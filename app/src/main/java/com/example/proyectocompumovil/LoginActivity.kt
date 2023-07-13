663ff3da8234164521cd6aa4efafa0e6de327ce2
Kenji Murakami <kenjimurakamirubio17@gmail.com>
2023-07-13 15:24:38 -0500

    Añadido de Librerías para servicios del Firebase, cambio del grandle y creación de las primeras variables globales

--- app/src/main/java/com/example/proyectocompumovil/LoginActivity.kt
+++ app/src/main/java/com/example/proyectocompumovil/LoginActivity.kt
@@ -2,8 +2,27 @@ package com.example.proyectocompumovil
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.LinearLayout
+import kotlin.properties.Delegates
 
 class LoginActivity : AppCompatActivity() {
+
+    companion object{
+        lateinit var useremail: String
+        lateinit var providerSession: String
+    }
+
+    //Variables globales
+    /* by Delegates.notNull significa que no puede ser
+    nulo, code de Kotlin*/
+    private var email by Delegates.notNull<String>()
+    private var password by Delegates.notNull<String>()
+
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var lyTerms: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
