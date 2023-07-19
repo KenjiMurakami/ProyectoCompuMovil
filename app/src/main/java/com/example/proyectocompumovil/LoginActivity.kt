1103c611afc159194da2da1f2f936c890b6f551d
Kenji Murakami <kenjimurakamirubio17@gmail.com>
2023-07-19 14:22:38 -0500

    Creación de las fun para el login y registro

--- app/src/main/java/com/example/proyectocompumovil/LoginActivity.kt
+++ app/src/main/java/com/example/proyectocompumovil/LoginActivity.kt
@@ -1,15 +1,23 @@
 package com.example.proyectocompumovil
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
+import java.util.Date
 import kotlin.properties.Delegates
 
 class LoginActivity : AppCompatActivity() {
 
-    companion object{
+    companion object {
         lateinit var useremail: String
         lateinit var providerSession: String
     }
@@ -29,5 +37,58 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        lyTerms = findViewById(R.id.lyTerms)
+        lyTerms.visibility = View.INVISIBLE
+
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+    fun login(view: View){
+        loginuser()
+    }
+
+    private fun loginuser(){
+        email = etEmail.text.toString()
+        password = etPassword.text.toString()
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){ task->
+                if(task.isSuccessful) goHome(email, "email")
+                else{
+                    if(lyTerms.visibility == View.INVISIBLE) lyTerms.visibility = View.VISIBLE
+                    else{
+                        var cbAcept = findViewById<CheckBox>(R.id.cbAcept)
+                        if(cbAcept.isChecked) register()
+                    }
+                }
+        }
+
+    }
+
+    private fun goHome(email: String, provider: String){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun register(){
+        email = etEmail.text.toString()
+        password = etPassword.text.toString()
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener {
+                if(it.isSuccessful){
+                    var dateRegister = SimpleDateFormat("dd/MM/yyyy").format(Date())
+                    var dbRegister = FirebaseFirestore.getInstance()
+                    dbRegister.collection("users").document(email).set(hashMapOf(
+                        "user" to email,
+                        "dataRegister" to dateRegister
+                    ))
+                    goHome(email,"email")
+                }
+                else Toast.makeText(this,"Ha ocurrido un error en el registro", Toast.LENGTH_SHORT).show()
+            }
     }
 }
