3d77461ecf35a3ac80741d6886edecb76927daa0
Kenji Murakami <kenjimurakamirubio17@gmail.com>
2023-07-20 00:08:08 -0500

    Métodos para la verificacion de la base de datos, comentado de código y direccionamiento a terminos y forgot password (DANIEL SANGAMA)

--- app/src/main/java/com/example/proyectocompumovil/LoginActivity.kt
+++ app/src/main/java/com/example/proyectocompumovil/LoginActivity.kt
@@ -46,10 +46,27 @@ class LoginActivity : AppCompatActivity() {
         mAuth = FirebaseAuth.getInstance()
     }
 
+    //verificacion en la base de datos
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null){
+            goHome(currentUser.email.toString(), currentUser.providerId)
+        }
+    }
+
+    override fun onBackPressed() {
+        val startMain = Intent(Intent.ACTION_MAIN)
+        startMain.addCategory(Intent.CATEGORY_HOME)
+        startMain.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(startMain)
+    }
+
     fun login(view: View){
         loginuser()
     }
 
+    //login de usuario al sistema para mandarlo a la página de inicio de la app
     private fun loginuser(){
         email = etEmail.text.toString()
         password = etPassword.text.toString()
@@ -67,6 +84,7 @@ class LoginActivity : AppCompatActivity() {
         }
 
     }
+
 
     private fun goHome(email: String, provider: String){
         val intent = Intent(this, MainActivity::class.java)
@@ -90,5 +108,14 @@ class LoginActivity : AppCompatActivity() {
                 }
                 else Toast.makeText(this,"Ha ocurrido un error en el registro", Toast.LENGTH_SHORT).show()
             }
+    }
+
+     fun goTerms(v: View){
+         val intent = Intent(this, TermsActivity::class.java)
+         startActivity(intent)
+     }
+
+    fun forgotPassword(view: View){
+        startActivity(Intent(this, ForgotPasswordActivity::class.java))
     }
 }
